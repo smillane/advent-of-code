10db8d4642e998ae6bf7a1d234d9a84fedf0da3f
Sean Millane <jmillaneiv@gmail.com>
2020-12-01 20:24:25 -0500

    Final Day 1, polishing

--- day 1/main.py
+++ day 1/main.py
@@ -1,13 +1,9 @@
 import itertools
-from functools import reduce
-
 
 find_sum = 2020
 
 with open("input.txt", "r") as input_file:
     input_list = [int(line) for line in input_file if line.strip()]
-
-print(input_list)
 
 for numbers in itertools.combinations(input_list, 2):
     if sum(numbers) == find_sum:
@@ -21,11 +17,13 @@ for numbers in itertools.combinations(input_list, 3):
 
 number_sum_part2 = input_list[number_position_part2[0]] * input_list[number_position_part2[1]] * input_list[number_position_part2[2]]
 
-print(input_list[number_position[0]])
-print(input_list[number_position[1]])
-print(number_sum)
-
-print(input_list[number_position_part2[0]])
-print(input_list[number_position_part2[1]])
-print(input_list[number_position_part2[2]])
-print(number_sum_part2)
+print("Part 1")
+print("Entry 1 is", input_list[number_position[0]])
+print("Entry 2 is", input_list[number_position[1]])
+print("Multiplied together is", number_sum)
+print()
+print("Part 2")
+print("Entry 1 is", input_list[number_position_part2[0]])
+print("Entry 2 is", input_list[number_position_part2[1]])
+print("Entry 3 is", input_list[number_position_part2[2]])
+print("Multiplied together is", number_sum_part2)
